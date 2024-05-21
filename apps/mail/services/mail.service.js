2af38e8a2dd168d0f04982d8834e76d3d07d9311
Asaf-Ida <Asafida20@gmail.com>
2024-05-21 18:25:03 +0300

    Init mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,33 @@
-// mail service
+import { utilService } from "../../../services/util.service.js"
+import { storageService } from "../../../services/async-storage.service.js"
+
+const email = { 
+    id: 'e101', 
+    subject: 'Miss you!', 
+    body: 'Would love to catch up sometimes', 
+    isRead: false, 
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'momo@momo.com', 
+    to: 'user@appsus.com' 
+}
+
+const loggedInUser = { 
+    email: 'user@appsus.com', 
+    fullname: 'Mahatma Appsus' 
+}
+
+const MAIL_KEY = 'mailsDB'
+_createMails()
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getDefaultFilter, 
+    getEmptyMail, 
+    getFilterFromSearchParams,
+}
+// For Debug (easy access from console):
+window.ms = mailService
