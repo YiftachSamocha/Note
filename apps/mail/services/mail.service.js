eb0e3b4bf1b4f14c05f36d9bd9ff7fcbf4592615
Asaf-Ida <Asafida20@gmail.com>
2024-05-22 14:22:47 +0300

    Continue Preparing Mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -32,3 +32,92 @@ export const mailService = {
 // For Debug (easy access from console):
 window.ms = mailService
 
+function query(filterBy = {}) {
+    return storageService.query(MAIL_KEY)
+        .then(mails => {
+            // if (filterBy.txt) {
+            //     const regExp = new RegExp(filterBy.txt, 'i')
+            //     mails = mails.filter(book => regExp.test(book.title))
+            // }
+
+            // if (filterBy.maxPrice) {
+            //     mails = mails.filter(book => book.listPrice.amount <= filterBy.maxPrice)
+            // }
+
+            return mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId)
+        .then(mail => {
+            mail = _setNextPrevMailId(mail)
+            return mail
+        })
+}
+
+function remove(mailId) {
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, mail)
+    } else {
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
+
+function getEmptyMail() {
+    const mail = {
+        id: utilService.makeId(), 
+        subject: '', 
+        body: '', 
+        isRead: false, 
+        sentAt: null,
+        removedAt: null,
+        from: null,
+        to: null
+    }
+    return mail
+}
+
+function getDefaultFilter(filterBy = { status: 'inbox', txt: '', labels: [] }) {
+    return filterBy
+}
+
+function getFilterFromSearchParams(searchParams) {
+    return {
+        status: searchParams.get('status') || '',
+        txt: searchParams.get('txt') || '',
+    }
+}
+
+function _createMails() {
+    const mails = []
+    for (let i = 0; i < 20; i++) {
+        const mail = {
+            id: utilService.makeId(),
+            subject: utilService.makeLorem(4),
+            body: utilService.makeLorem(60),
+            isRead: Math.random() > 0.5,
+            sentAt: utilService.getRandomIntInclusive(1551133130200, 1551133930700),
+            removedAt: null,
+            from: utilService.makeLorem(1) + '@' + utilService.makeLorem(1) + '.com',
+            to: 'user@appsus.com'
+        } 
+        mails.push(mail)
+    } 
+    utilService.saveToStorage(MAIL_KEY, mails)
+}
+
+function _setNextPrevMailId(mail) {
+    return storageService.query(MAIL_KEY).then((mails) => {
+        const mailIdx = mails.findIndex((currMail) => currMail.id === mail.id)
+        const nextMail = mails[mailIdx + 1] ? mails[mailIdx + 1] : mails[0]
+        const prevMail = mails[mailIdx - 1] ? mails[mailIdx - 1] : mails[mails.length - 1]
+        mail.nextMailId = nextMail.id
+        mail.prevMailId = prevMail.id
+        return mail
+    })
+}
