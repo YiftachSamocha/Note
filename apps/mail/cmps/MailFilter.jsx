b7d8270a2fd30c9b57297ee781119d1c11c248e1
Asaf-Ida <Asafida20@gmail.com>
2024-05-24 18:22:51 +0300

    Add filtering by text and read status

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,5 +1,39 @@
-export function MailFilter() {
-    return (
-        <input type="text" placeholder="Search"/>
-    )
+const { useState, useEffect, useRef } = React
+
+export function MailFilter({ filterBy, onFilter }) {
+    const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
+    // const readMail = useRef('')
+
+    useEffect(() => {
+        onFilter(filterByToEdit)
+    }, [filterByToEdit])
+
+    function handleChange({ target}) {
+        const { name, value } = target
+
+        // switch (name) {
+        //     case 'readStatus':
+        //         readMail.current = (readMail.current === 'read' || readMail.current === '') ? 'unread' : 'read'
+        //         value = readMail.current
+        //         break;
+        //     case 'txt':
+        //         value = target.value
+        // }
+
+        setFilterByToEdit(prevFilterBy => ({ ...prevFilterBy, [name]: value}))
+    }
+
+    return <section className="mail-filter">
+        <div className="search-box">
+            <label htmlFor="txt"><img src="../../assets/img/search.png"/></label>
+            <input value={filterByToEdit.txt} onChange={handleChange} name="txt" type="text" placeholder="Search" id="txt"/>
+        </div>
+
+            {/* <button onClick={(ev) => handleChange(ev)} name="readStatus">Read / Unread</button> */}
+            <select name="readStatus" onChange={handleChange}>
+                <option value="">All</option>
+                <option value="read">Read Mails</option>
+                <option value="unread">Unread Mails</option>
+            </select>
+    </section>
 }
--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,6 +1,8 @@
 import { MailPreview } from "./MailPreview.jsx"
 
 export function MailList(props) {
+    if (props.mails.length === 0) return 'No mails to show...'
+
     return (
     <section className="mail-list">
         <ul>
