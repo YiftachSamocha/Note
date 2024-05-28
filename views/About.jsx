bc58947d090a81cfadc21c64297ca5df3938e41c
Yiftach Samocha <yiftachsamo@gmail.com>
2024-05-28 16:06:58 +0300

    Style app header; Create home page; Create about page

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,24 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <div className="profile">
+            <img src="./assets/img/yiftach.jpeg" />
+            <h3>Yiftch Samocha</h3>
+            <p>Yiftach Samocha started his coding journey by hacking his school's vending machine to
+                dispense unlimited chocolate bars. Since then, he’s mastered JavaScript, Python, HTML, and CSS,
+                all while drinking cold coffee at 3 AM. Yiftach’s proudest title is "Chief Bug Creator"
+                at top tech firms, where he made user interfaces as confusing as possible.
+                In his spare time, he organizes digital cat photos and dreams of creating an app
+                that makes his bed every morning.</p>
+        </div>
+        <div className="profile">
+            <img src="./assets/img/asaf.jpg" />
+            <h3>Asag Ida</h3>
+            <p>Asaf Ida's coding adventure began with an AI that wanted world domination but now organizes hIS socks.
+                Fluent in C++ to JavaScript, Asaf excels at writing "Hello World" in every language.
+                His career highlights include creating a social media platform for goldfish and translating
+                cat meows into Shakespearean English. Outside of work, he wanders Stack Overflow and dreams
+                of the next big tech joke.</p>
+        </div>
     </section>
 }
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,17 @@
+const { Link} = ReactRouterDOM
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1>Appsus</h1>
+        <section className="app-links">
+                <Link to="/mail">
+                    <i className="fa-regular fa-envelope"></i>
+                    <p>Mail</p>
+                </Link>
+                <Link to="/note">
+                    <i className="fa-regular fa-note-sticky"></i>
+                    <p>Note</p>
+                </Link>
+            </section>
     </section>
 }
