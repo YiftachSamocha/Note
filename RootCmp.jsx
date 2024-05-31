83fb02b61c5e421e423e47e10be644ecff08623a
Yiftach Samocha <yiftachsamo@gmail.com>
2024-05-31 17:37:21 +0300

    Add book app

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -8,19 +8,32 @@ import { MailIndex } from "./apps/mail/views/MailIndex.jsx"
 import { NoteIndex } from "./apps/note/views/NoteIndex.jsx"
 import { UserMsg } from "./cmps/UserMsg.jsx"
 import { MailDetails } from "./apps/mail/pages/MailDetails.jsx"
+import { BookIndex } from "./apps/book/views/BookIndex.jsx"
+import { BookEdit } from "./apps/book/views/BookEdit.jsx"
+import { BookDetails } from "./apps/book/views/BookDetails.jsx"
+
 
 export function App() {
     return <Router>
         <section className="app">
             <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/mail" element={<MailIndex />}>
-                    <Route path="/mail/:mailId" element={<MailDetails />} />
-                </Route>
-                <Route path="/note" element={<NoteIndex />} />
-            </Routes>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/mail" element={<MailIndex />}>
+                        <Route path="/mail/:mailId" element={<MailDetails />} />
+                    </Route>
+
+                    <Route path="/note" element={<NoteIndex />} />
+
+                    <Route path='/book' element={<BookIndex />} />
+                    <Route path='/book/edit' element={<BookEdit />} />
+                    <Route path='/book/edit/:bookId' element={<BookEdit />} />
+                    <Route path='/book/:bookId' element={<BookDetails />} />
+
+                </Routes>
+            </main>
             <UserMsg />
         </section>
     </Router>
